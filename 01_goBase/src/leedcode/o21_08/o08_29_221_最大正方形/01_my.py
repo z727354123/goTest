1da8b356d12e519d46535db2fa875fcb8e42a92b
judy <7273654123@qq.com>
2021-08-30 00:09:59 +0800

    原 List 开整

--- 01_goBase/src/leedcode/o21_08/o08_29_221_最大正方形/01_my.py
+++ 01_goBase/src/leedcode/o21_08/o08_29_221_最大正方形/01_my.py
@@ -5,25 +5,28 @@ class Solution:
 	def maximalSquare(self, matrix: List[List[str]]) -> int:
 		# 获取长度
 		rowLen, colLen = len(matrix), len(matrix[0])
-		# 记录结果
-		tmpList = [[0] * colLen for _ in range(rowLen)]
 		# 定义结果
 		max = 0
 		# 遍历内容
 		for row in range(rowLen):
 			if matrix[row][0] == "1":
-				tmpList[row][0] = max = 1
+				matrix[row][0] = max = 1
+			else:
+				matrix[row][0] = 0
 		for col in range(1, colLen):
 			if matrix[0][col] == "1":
-				tmpList[0][col] = max = 1
+				matrix[0][col] = max = 1
+			else:
+				matrix[0][col] = 0
 		# 开始计算
 		for row in range(1, rowLen):
 			for col in range(1, colLen):
 				if matrix[row][col] == "0":
+					matrix[row][col] = 0
 					continue
 				# 取最小值 + 1
-				tmpList[row][col] = lastVal = min(tmpList[row - 1][col - 1], tmpList[row - 1][col],
-				                                  tmpList[row][col - 1]) + 1
+				matrix[row][col] = lastVal = min(matrix[row - 1][col - 1], matrix[row - 1][col],
+				                                  matrix[row][col - 1]) + 1
 				if lastVal > max:
 					max = lastVal
 		return max * max
