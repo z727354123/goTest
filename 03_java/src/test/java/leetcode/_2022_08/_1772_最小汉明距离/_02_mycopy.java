15a7cbbf1527f543ee9d5b39d5f6fe7dc6195728
judy <7273654123@qq.com>
2022-08-05 23:26:25 +0800

    找到问题点

--- 03_java/src/test/java/leetcode/_2022_08/_1772_最小汉明距离/_02_mycopy.java
+++ 03_java/src/test/java/leetcode/_2022_08/_1772_最小汉明距离/_02_mycopy.java
@@ -8,11 +8,15 @@ import java.util.stream.IntStream;
 public class _02_mycopy {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] source = {1, 2, 3, 4};
-        int[] target = {2, 1, 4, 5};
-        int[][] allowedSwaps = {{0, 1}, {2, 3}};
-        solution.minimumHammingDistance(source, target, allowedSwaps);
+        int[] source = {41, 37, 51, 100, 25, 33, 90, 49, 65, 87, 11, 18, 15, 18};
+
+
+        int[] target = {41, 92, 69, 75, 29, 13, 53, 21, 17, 81, 33, 19, 33, 32};
+        int[][] allowedSwaps = {{0, 11}, {5, 9}, {6, 9}, {5, 7}, {8, 13}, {4, 8}, {12, 7}, {8, 2}, {13, 5}, {0, 7}, {6, 4}, {8, 9}, {4, 12}, {6, 1}, {10, 0}, {10, 2}, {7, 3}, {11, 10}, {5, 2}, {11, 1}, {3, 0}, {8, 5}, {12, 6}, {2, 1}, {11, 2}, {4, 9}, {2, 9}, {10, 6}, {12, 10}, {4, 13}, {13, 2}, {11, 9}, {3, 6}, {0, 4}, {1, 10}, {5, 11}, {12, 1}, {10, 4}, {6, 2}, {10, 7}, {3, 13}, {4, 5}, {13, 10}, {4, 7}, {0, 12}, {9, 10}, {9, 3}, {0, 5}, {1, 9}, {5, 10}, {8, 0}, {12, 11}, {11, 4}, {7, 9}, {7, 2}, {13, 9}, {12, 3}, {8, 6}, {7, 6}, {8, 12}, {4, 3}, {7, 13}, {0, 13}, {2, 0}, {3, 8}, {8, 1}, {13, 6}, {1, 4}, {0, 9}, {2, 3}, {8, 7}, {4, 2}, {9, 12}};
+        int val = solution.minimumHammingDistance(source, target, allowedSwaps);
+        System.out.println(val);
     }
+
     static
     class Solution {
         public int minimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps) {
@@ -55,6 +59,7 @@ public class _02_mycopy {
                         if (srcCount > tarCount) {
                             ans += srcCount - tarCount;
                         }
+                        tarCountMap.remove(key);
                     }
                 }
             }
@@ -62,7 +67,7 @@ public class _02_mycopy {
         }
 
         private void incre(Map<Integer, Integer> item, int source) {
-            item.put(source, item.getOrDefault(source, 1));
+            item.put(source, item.getOrDefault(source, 0) + 1);
         }
 
         // 并查集
@@ -73,6 +78,7 @@ public class _02_mycopy {
             public UniFind(int len) {
                 this.root = IntStream.range(0, len).toArray();
             }
+
             // 找到父节点
             public int find(int num) {
                 int fa = root[num];
