c8e849850783e56bcfac9ada47afcd9fdcf9f9c7
judy <7273654123@qq.com>
2022-09-03 12:02:22 +0800

    有点问题

---
+++ 03_java/src/test/java/leetcode/_2022_09/_002_2202_K次操作后最大化顶端元素/_01_myans.java
@@ -0,0 +1,18 @@
+package leetcode._2022_09._002_2202_K次操作后最大化顶端元素;
+
+
+public class _01_myans {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+    }
+
+    static
+    /**
+     *
+     */
+    class Solution {
+
+    }
+
+}
